676c4e3515395d4d1609935f2beb6d25c29b684a
Usuario <Usuario@DESKTOP-5N0MR6R>
2024-04-25 13:10:51 -0300

    Agregar archivos de proyecto.

---
+++ ListaProducts/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaProducts
+{
+    public partial class Form1 : Form
+    {
+        private int n = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            // Adicionamos un nuevo reglon 
+            int n = dtgvProductos.Rows.Add();
+
+            // Colocamos la informacion
+            dtgvProductos.Rows[n].Cells[0].Value = txtCodigo.Text;
+            dtgvProductos.Rows[n].Cells[1].Value = txtNombre.Text;
+            dtgvProductos.Rows[n].Cells[2].Value = txtPrecio.Text;
+
+            // Limiamos los txt
+            txtCodigo.Text = "";
+            txtNombre.Text = "";
+            txtPrecio.Text = "";
+        }
+
+        private void dtgvProductos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            n = e.RowIndex;
+
+            if (n!=-1)
+            {
+
+                lblInformacion.Text = (string)dtgvProductos.Rows[n].Cells[1].Value;
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (n != -1) 
+            {
+                dtgvProductos.Rows.RemoveAt(n);
+            
+            }
+        }
+    }
+}
